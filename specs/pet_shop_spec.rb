7e1540d9fd914a8a829d9ee3d9fe2d6f576693ef
John Taggart <taggartj1985@gmail.com>
2020-03-13 16:31:24 +0000

    pets sold increased

--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -109,7 +109,7 @@ class TestPetShop < Minitest::Test
   #   sold = pets_sold(@pet_shop)
   #   assert_equal(0, sold)
   # end
-
+# done
   # def test_increase_pets_sold
   #   increase_pets_sold(@pet_shop,2)
   #   sold = pets_sold(@pet_shop)
